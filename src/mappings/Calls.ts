3293778717ffeae87a0635187bd6c4be4c5af661
JQQQ <jiqiang90@hotmail.com>
2021-03-16 16:22:28 +1300

    extrinsic call parent_id should be null

--- src/mappings/Calls.ts
+++ src/mappings/Calls.ts
@@ -2,26 +2,28 @@ import {SubstrateExtrinsic} from "@subql/types";
 import {Call} from "../types/models/Call";
 import {Vec} from "@polkadot/types"
 import {AnyTuple, CallBase} from "@polkadot/types/types"
+import {flatDeep} from "./utils/flatten";
 
-function extractCalls(call: CallBase<AnyTuple>, id: number, parentCallId:string): Call[]{
+function extractCalls(call: CallBase<AnyTuple>, id: number, parentCallId:string,isExtrinsic:boolean): Call[]{
     const callId = `${parentCallId}-${id}`
     const entity = new Call(callId);
+    const entities = [] as Call[];
     entity.method = call.method
     entity.module = call.section;
-    entity.parentCallId = parentCallId;
+    entity.parentCallId = isExtrinsic? null: parentCallId;
+    entities.push(entity);
     if (call.method == 'batchAll' && call.section == 'utility'){
         const calls = call.args[0] as Vec<CallBase<AnyTuple>>
-        return [].concat.apply([],(calls.map((call,idx) =>
-            extractCalls(call,idx,callId)
-        )))
+        return entities.concat(flatDeep((calls.map((call,idx) =>
+            extractCalls(call,idx,callId,false)
+        )),Infinity));
     }else{
-        return [];
+        return entities;
     }
-
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void>{
-    const calls =  extractCalls(extrinsic.extrinsic.method,extrinsic.idx,extrinsic.block.block.header.number.toString())
+    const calls =  extractCalls(extrinsic.extrinsic.method,extrinsic.idx,extrinsic.block.block.header.number.toString(),true)
     await Promise.all(calls.map((call) => call.save()))
 
 }
